82fc60b80cf9d868c3fe773273117fb61f3aae01
Jay Chen <webmaster@webther.com>
2018-07-29 20:24:27 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <display-code :type="'rawhtml'" :title="'Raw HTML code'" :code="htmlcode"></display-code>
+    <display-code :type="'html'" :title="'HTML code'" :code="htmlcode"></display-code>
+    <display-code :type="'js'" :title="'JavaScript code'" :code="jscode"></display-code>
+    <display-code :type="'php'" :title="'PHP code'" :code="phpcode"></display-code>
+ 
+    <hr />
+    <br />
+
+    <display-code :title="'Raw HTML code'" :type="'rawhtml'" :code="htmlcode">
+      <template slot="titleSlot" slot-scope="props">
+        <h3 class="title">{{ props.title }}</h3>
+      </template>
+      <template slot="codeSlot" slot-scope="props">
+        <pre>{{ props.code }}</pre>        
+      </template>
+    </display-code>
+  </div>
+</template>
+
+<script>
+import DisplayCode from './components/DisplayCode'
+
+export default {
+  name: 'App',
+  components: {
+    DisplayCode
+  },
+  data: function () {
+    return {
+      htmlcode: `<!-- My comment -->
+<div style="color: green; font-size: 16px;">
+  <div><strong>Hello World!</strong></div>
+  <button onclick="alert('Good morning!');">Greeting!</button>
+</div>
+`,
+      phpcode: `<?php
+  function greeting () {
+    return 'Hello World!';  
+  };
+?>`,
+      jscode: `<!-- My comment -->
+<script>
+  alert('hi');
+<\/script>
+`
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/DisplayCode.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="display-code">
+    <slot></slot>
+    <slot name="titleSlot" :title="title">
+      <h3 class="title">{{ title }}</h3>
+    </slot>
+    <slot name="codeSlot" :code="code">
+      <div v-if="type == 'html'">
+        <div v-html="code"></div>
+      </div>
+      <div v-if="type == 'rawhtml'" :code="code">
+        <pre>{{ code }}</pre>
+      </div>
+      <div v-if="type == 'php'" :code="code">
+        <pre>{{ code }}</pre>
+      </div>
+      <div v-if="type == 'js'" :code="code">
+        <pre>{{ code }}</pre>
+      </div>
+    </slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DisplayCode',
+  props: {
+    type: {
+      type: String,
+      require: true
+    },
+    title: String,
+    code: String
+  }
+};
+</script>
+
+<style scoped>
+.display-code {
+  margin: 0 0 30px 0;
+}
+.display-code h3.title {
+  margin: 0 0 15px 0;
+}
+.display-code pre {
+  border: 1px solid #ccc;
+  padding: 15px;
+  background: #ededed;
+}
+</style>
